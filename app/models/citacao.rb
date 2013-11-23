a279f121e59fa030558d16e33e5e79235365d7d6
Joao Vortmann <jvortmann@gmail.com>
2013-11-23 16:58:23 -0200

    Add formatacao to citacao.

--- app/models/citacao.rb
+++ app/models/citacao.rb
@@ -2,4 +2,8 @@ class Citacao < ActiveRecord::Base
   belongs_to :livro
 
   validates :livro, presence: true
+
+  def formata
+  	"#{self.trecho} [#{self.livro.autores.upcase}, #{self.livro.ano_da_publicacao}]"
+  end
 end
