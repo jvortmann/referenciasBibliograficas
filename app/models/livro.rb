53438112300a0845bac734e365244ad54d44b621
Joao Vortmann <jvortmann@gmail.com>
2013-11-23 16:52:03 -0200

    Add abnt format to livro.

--- app/models/livro.rb
+++ app/models/livro.rb
@@ -2,4 +2,8 @@ class Livro < ActiveRecord::Base
   has_many :citacaos
 
   validates :ano_da_publicacao, numericality: { only_integer: true, greater_than: 0 }
+
+  def abnt
+  	"#{self.autores}. #{self.titulo}. #{self.cidade}: #{self.editora}, #{self.ano_da_publicacao}."
+  end
 end
